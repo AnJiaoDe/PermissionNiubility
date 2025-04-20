ff77803e380080dce97006a9fe9c368c0b19ebb6
CY <437042721@qq.com>
2025-04-20 16:54:22 +0800

    update

--- permissionniubility/src/main/java/com/cy/permissionniubility/PermissionUtils.java
+++ permissionniubility/src/main/java/com/cy/permissionniubility/PermissionUtils.java
@@ -56,6 +56,7 @@ public class PermissionUtils {
         showDialog(context, text_ask, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
                 context.startActivity(intent);
             }
         });
